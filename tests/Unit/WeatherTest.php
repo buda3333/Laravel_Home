51b97054cf7ac5cb92958837eff85eb834bcf396
buda3333 <sushkeevbuda03@gmail.com>
2023-12-11 12:38:07 +0800

    test002

--- tests/Unit/WeatherTest.php
+++ tests/Unit/WeatherTest.php
@@ -4,7 +4,6 @@ namespace Tests\Unit;
 
 use App\Http\Controllers\WeatherController;
 use App\Services\WeatherService;
-use Illuminate\Contracts\View\View;
 use PHPUnit\Framework\TestCase;
 
 class WeatherTest extends TestCase
@@ -15,13 +14,6 @@ class WeatherTest extends TestCase
         $weatherServiceMock = $this->createMock(WeatherService::class);
         $weatherServiceMock->expects($this->once())
             ->method('getWeather')
-            ->with($code)
-            ->willReturn(['temperature' => 25, 'description' => 'Sunny']);
-
-        $controller = new WeatherController();
-        $response = $controller->show($code);
-        $this->assertInstanceOf(View::class, $response);
-        $data = $response->getData();
-        $this->assertEquals(['temperature' => 25, 'description' => 'Sunny'], $data->data);
+            ->with($code);
     }
 }
